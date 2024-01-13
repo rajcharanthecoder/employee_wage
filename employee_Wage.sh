2dd1a4ac8ab902a68ba9dae16d21bb3bbcadb31d
GANGISETTY RAJ CHARAN <gq5298@srmist.edu.in>
2024-01-13 12:23:46 +0530

    Feature Added: Daily Wage Functionality Added

--- employee_Wage.sh
+++ employee_Wage.sh
@@ -6,3 +6,12 @@ echo "Employee is Present"
 else
 echo "Absent"
 fi
+
+wage_per_hour=20
+full_day_hours=8
+
+# Calculate daily wage
+daily_wage=$((wage_per_hour * full_day_hours))
+
+# Display the result
+echo "Daily Employee Wage: $daily_wage"
