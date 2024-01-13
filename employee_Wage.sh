d8cdda02e7c5aa0713adb9cee1098598a0b9f6f9
GANGISETTY RAJ CHARAN <gq5298@srmist.edu.in>
2024-01-13 12:35:49 +0530

    Feature Added : Part Time Wage Calculator

--- employee_Wage.sh
+++ employee_Wage.sh
@@ -15,3 +15,12 @@ daily_wage=$((wage_per_hour * full_day_hours))
 
 # Display the result
 echo "Daily Employee Wage: $daily_wage"
+
+part_time_hours=4
+wage_per_hour=20
+
+# Function to calculate daily wage
+
+part_time_wage=$((part_time_hours*wage_per_hour))
+
+echo "Part-time Employee Daily Wage: $part_time_wage"
